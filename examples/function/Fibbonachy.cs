f71069ba30d77b0827f33746740c8a81196d9c44
Vasyliy I. Gurianov <vg2007sns@rambler.ru>
2017-12-24 20:23:32 +0300

    Add files via upload

---
+++ examples/function/Fibbonachy.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sequence
+{
+    /* Problem by Fibbonachy
+     * 
+     * Un+2 = Un+1 + Un, U1 = U2 = 1
+     * 1, 1, 2, 3, 5, 8, 13, ...
+     */
+
+    // ** Concept = Couple of rabbits (hares)
+    public class Node
+    { 
+        public string val;
+        public int val1;
+
+        public bool state;  // Adult(mature) or childish couple of rabbits
+        public Node next; 
+        public Node previous;
+        public Node()
+        {
+            val = "?";
+            next = null; previous = null;
+            state = false;
+        }
+    }
+    public class Dependencis
+    {
+        public Node x, t;
+        public Node y, f;
+
+        public string probe;
+        int ix, iy;
+
+        public Dependencis()
+        {
+            // ** x is a month
+            x = new Node(); x.val1 = 0;
+            t = x;
+            for (int i = 0; i < 12; i++)
+            {
+                t.next = new Node(); t.val1 = i + 1;
+                //t.next.previous = t; 
+                t = t.next;
+            }
+            ix = 0; iy = 0;
+            t = x;
+            // ** y is the couple of rabbits (hares)
+            y = new Node(); y.state = false;
+            y.next = new Node(); y.next.state = true;
+        }
+        public void Run()
+        {
+            fbSequence();
+        }
+        public void fbSequenceNumeric()
+        {
+            int fb0, fb1, fb;
+            fb0 = 1; fb1 = 1;
+            for (int i = 3; i<12; i++) {
+                fb = fb0 + fb1;
+                fb0 = fb1; fb1 = fb;
+            }
+
+        }
+        public void fbSequence()
+        {
+            if (t != null)
+            {
+                ix++;
+                Node tt = y; int iy = 0; int yy = 0; Node yt;
+                do
+                {
+                    if (tt.state)
+                    {
+                        iy++;
+                        //y.previous = new Node(); yt = y0; y = y.previous; y.next = yt;
+                        yt = new Node(); yt.next = y; y = yt; }
+                    else { tt.state = true; };
+
+                tt = tt.next; yy++;
+                } while (tt != null);
+
+                t = t.next; 
+                probe = ix.ToString() + ", " + iy.ToString()+ ", " + yy.ToString();
+            }
+            else probe = "The end";
+        }
+
+    } // public class Dependencis
+}
